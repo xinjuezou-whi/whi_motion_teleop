1cdd1467d0cb5eeed490f663b217294f88f45a40
Xinjue Zou <hit_zou@126.com>
2023-08-02 11:05:36 +0800

    add sh file for shotcut

---
+++ launch/WHI_teleop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/whi/catkin_workspace/
+source /opt/ros/melodic/setup.bash
+source /home/whi/catkin_workspace/devel/setup.bash
+echo "launching application, please wait..."
+roslaunch whi_motion_ctrl_teleop teleop.launch
