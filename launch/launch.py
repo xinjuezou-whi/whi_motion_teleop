7174c9fc827561cca575cd8170610df1e4efe391
Xinjue <hit_zou@126.com>
2026-02-03 12:55:47 +0800

    revise to support namespace

--- launch/launch.py
+++ launch/launch.py
@@ -13,11 +13,16 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
+    # Input parameters declaration
+    namespace = LaunchConfiguration('namespace')
     use_stamped_vel = LaunchConfiguration('use_stamped_vel')
 
     config_file = PathJoinSubstitution([
@@ -26,16 +31,29 @@ def generate_launch_description():
         'config.yaml'
     ])
 
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=config_file,
+            root_key=namespace,
+            param_rewrites={},
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
+
     return LaunchDescription([
+        DeclareLaunchArgument('namespace', default_value='', description='Top-level namespace'),
         DeclareLaunchArgument('use_stamped_vel', default_value='true', description='Flag of use stamped twist'),
         Node(
             package='whi_motion_teleop',
             executable='whi_motion_teleop_node',  # <-- must match your CMake target name!
             name='whi_motion_teleop',
+            namespace=namespace,
             output='screen',
             emulate_tty=True,
             parameters=[
-                config_file,
+                configured_params,
                 {'use_stamped_vel': LaunchConfiguration('use_stamped_vel')} # do not define in yaml if it is dynamic through argument
             ],
         )
